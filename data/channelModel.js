28b653bd8faf41c9723db26ffa8d537557e2ede1
Sori <chan8919@naver.com>
2025-04-29 19:55:17 +0900

    checkLimutChannelNum 추가가

--- data/channelModel.js
+++ data/channelModel.js
@@ -1,34 +1,78 @@
 const conn = require('./mariadb');
+const users = require('./memberModel');
 
-function findByUserEmail({email}){
-    const sql = 'SELECT c.*,m.email FROM channels AS c JOIN members AS m ON c.member_id = m.id where m.email = ?';
-    return new Promise((resolve,reject)=>{
-        conn.query(sql,email,(err,results)=>{
-            if(err){
+// 이메일에 해당하는 채널 목록 조회회
+function findByUserEmail({ email }) {
+
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT c.*,m.email FROM channels AS c JOIN members AS m ON c.member_id = m.id where m.email = ?';
+        conn.query(sql, email, (err, results) => {
+            if (err) {
                 reject(err);
-            }else{
+            } else {
                 resolve(results);
             }
         })
     })
 
 }
+// 모든 채널 조회회
 function getAllChannels() {
-    const sql = 'SELECT * FROM channels';
-    return new Promise((resolve,reject)=>{
+
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM channels';
         conn.query(sql, function (err, results, fields) {
-            if(err){
+            if (err) {
                 reject(err);
             }
-            else{
+            else {
                 resolve(results);
             }
         })
     })
-   
+
+}
+//채널 추가
+async function addChannel({ name, desc, email }) {
+    try {
+        const user = await users.findByEmail({ "email": email });
+        return new Promise((resolve, reject) => {
+            const sql = 'INSERT INTO members (name,member_id,desc) VALUES (?,?,?)';
+            conn.query(sql, [name, user.id, desc],
+                function (err, results) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(true);
+                    }
+                }
+            )
+        })
+    }
+    catch (err) {
+        return err;
+    }
+
+}
+// 제한 확인인
+function checkLimitChannelNum({email}) {
+    const channelLimit = 100;
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT COUNT(*) AS count FROM channels AS c JOIN members AS u ON c.member_id=u.id WHERE u.email = ? ';
+        conn.query(sql, email, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results[0].count);
+                resolve(results[0].count);
+            }
+        })
+    })
+
 }
 
 module.exports = {
     findByUserEmail,
-    getAllChannels
+    getAllChannels,
+    addChannel
 };
